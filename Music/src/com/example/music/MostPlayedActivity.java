9943c493ff9b398b5d834882e542e438310ec89b
Mohamed Aboul Azm <mohamedtamer92@gmail.com>
2013-12-26 13:22:03 +0200

    added tabs and tested

---
+++ Music/src/com/example/music/MostPlayedActivity.java
@@ -0,0 +1,20 @@
+package com.example.music;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class MostPlayedActivity extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.activity_most_played, container, false);
+         
+        return rootView;
+    }
+ 
+}
---
+++ Music/src/com/example/music/ThirdActivity.java
@@ -0,0 +1,20 @@
+package com.example.music;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class ThirdActivity extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.activity_third, container, false);
+         
+        return rootView;
+    }
+ 
+}
