a541f51733eaacd2bbe46a739e5119a7a0bad3a5
Khloud-Khalid <khloud.khalid7@gmail.com>
2014-01-06 18:00:16 +0200

    yaraab ne5las!

--- Music/src/com/example/music/MostPlayedActivity.java
+++ Music/src/com/example/music/MostPlayedActivity.java
@@ -30,37 +30,40 @@ System.out.println(files.getItem(0));
          
         return rootView;
     }
-    public static List<String> searchAndDisplay(ArrayList<String> arr) {
+    public static ArrayList<String> searchAndDisplay(ArrayList<String> arr) {
 
-        ArrayList<String> list1 = new ArrayList();
-        ArrayList<Integer> list2 = new ArrayList();
-        ArrayList<String> list3 = new ArrayList();
-        for (int i = 0; i < arr.size(); i++) {
-            int index = list1.indexOf(arr.get(i));
-            if (index != -1) {
-                    int newCount = list2.get(index)+1;
-              list2.set(index,newCount);
-            } else {
-                       list1.add(arr.get(i));
-                   list2.set(list1.size()-1, 1);
-            }
-        }
-        while(list3.size()<25 && list3.size()<= list2.size()){
-                if(!list2.isEmpty()){
-                        
-        int maxCount = 0;
-        int index = -1;
-        for (int i = 0; i < list2.size(); i++) {
-            if (maxCount < list2.get(i)) {
-                maxCount = list2.get(i);
-                index = i;
-            }
-        }
-        if(index != -1){
-        list3.add(list1.get(index));
-        list1.remove(index);
-        list2.remove(index);
-        }
-        }}
- return list3;
-}}
+		ArrayList<String> list1 = new ArrayList();
+		ArrayList<Integer> list2 = new ArrayList();
+		ArrayList<String> list3 = new ArrayList();
+		for (int i = 0; i < arr.size(); i++) {
+			if (list1.contains(arr.get(i))) {
+				int count = list2.get(list1.indexOf(arr.get(i)));
+				list2.set(list1.indexOf(arr.get(i)),
+						count + 1);
+			} else {
+				list1.add(arr.get(i));
+				list2.add(1);
+			}
+		}
+
+		while (list3.size() < 25 && list3.size() <= list2.size()) {
+			if (!list2.isEmpty()) {
+
+				int maxCount = 0;
+				int index = -1;
+				for (int i = 0; i < list2.size(); i++) {
+					if (maxCount < list2.get(i)) {
+						maxCount = list2.get(i);
+						index = i;
+					}
+				}
+				if (index != -1) {
+					list3.add(list1.get(index));
+					list1.remove(index);
+					list2.remove(index);
+				}
+			}
+		}
+		return list3;
+	}
+    }
